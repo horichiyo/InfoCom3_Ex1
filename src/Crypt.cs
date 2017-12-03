235d29291f8d487657eba425ea9d577eb0db26f0
yo <sun4geek@gmail.com>
2017-12-03 17:26:24 +0900

    [Add] C#codes
    [Add] C#codes

---
+++ src/Crypt.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ex1
+{
+	public static class Crypt
+	{
+		//暗号化(DES)関数
+		public static byte[] EncryptText(string str, byte[] Key)
+		{
+			DESCryptoServiceProvider des = new DESCryptoServiceProvider()
+			{
+				Key = Key,
+				IV = Key
+			};
+			byte[] bstr = Encoding.UTF8.GetBytes(str);
+
+			ICryptoTransform transform = des.CreateEncryptor();
+			byte[] encrypted = transform.TransformFinalBlock(bstr, 0, bstr.Length);
+			transform.Dispose();
+			return encrypted;
+		}
+
+		//暗号化(DES)関数
+		public static byte[] EncryptText(byte[] bstr, byte[] Key)
+		{
+			DESCryptoServiceProvider des = new DESCryptoServiceProvider()
+			{
+				Key = Key,
+				IV = Key
+			};
+
+			ICryptoTransform transform = des.CreateEncryptor();
+			byte[] encrypted = transform.TransformFinalBlock(bstr, 0, bstr.Length);
+			transform.Dispose();
+			return encrypted;
+		}
+
+		//復号化(DES)関数
+		public static byte[] DecryptText(byte[] bcrypt, byte[] Key)
+		{
+			DESCryptoServiceProvider des = new DESCryptoServiceProvider()
+			{
+				Key = Key,
+				IV = Key
+			};
+
+			ICryptoTransform destransform = des.CreateDecryptor();
+			byte[] decrypted = destransform.TransformFinalBlock(bcrypt, 0, bcrypt.Length);
+			destransform.Dispose();
+			return decrypted;
+
+		}
+		//Caisar関数(mode True:暗号化 False:復号化)
+		public static byte[] CaiserCipher(byte[] str, int shiftSize, Boolean mode)
+		{
+			var i = shiftSize;
+			byte[] result = new byte[str.Length];
+			if(!mode){
+				i *= -1;
+			}
+			for(var j = 0; j < str.Length; j++){
+				result[j] = (byte)((256 * shiftSize / 256 + (str[j] + shiftSize)) % 256);
+			}
+			return result;
+		}
+	}
+}
---
+++ src/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ex1
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			byte[] secretKey = {
+				0x13,
+				0xCB,
+				0x73,
+				0xBE,
+				0xA1, 
+				0xC1, 
+				0xED,
+				0x5B
+			};
+
+			//引数で指定したテキストを暗号化（DES暗号）
+			string textData = "The majestic red rock landscape here is awe inspiring,\nand I want to capture it with my camera.\nBut the area is so vast that I can only see a part of it with my lens.";
+			byte[] encryptdata = Crypt.EncryptText(textData, secretKey);
+
+			//復号化（DES暗号）
+			byte[] decryptdata = Crypt.DecryptText(encryptdata, secretKey);
+			if(textData.Equals(Encoding.GetEncoding(50220).GetString(decryptdata))){
+				Console.WriteLine(textData);
+				Console.WriteLine("文字列復号化完了");
+				Console.WriteLine("-以下暗号化したあとの文字列---------------");
+				Console.WriteLine(Encoding.GetEncoding(50220).GetString(encryptdata));
+				Console.WriteLine("-以上暗号化したあとの文字列---------------");
+			}
+
+			//BMP画像の暗号化（Caiser，DES）
+			byte[] result;
+			byte[] resultDes;
+			var fileName = "Lenna.bmp";
+
+			//Caiserで画像を暗号化
+			Bmp.Load(fileName, out Bmp.BITMAPFILEHEADER bfh, out Bmp.BITMAPINFOHEADER bih, out byte[] bitData);
+			result = Crypt.CaiserCipher(bitData, 3, true);
+			Bmp.Save("resultCaiser.bmp", bih.biWidth, bih.biHeight, bih.biBitCount, result);
+
+			//BMP画像の暗号化（DES）
+			Bmp.Load(fileName, out bfh, out bih, out bitData);
+			resultDes = Crypt.EncryptText(bitData, secretKey);
+			for(int i = 0; i < result.Length; i++){
+				result[i] = resultDes[i];
+			}
+
+			//BMP画像の復号化（DES）
+			if(Bmp.Save("resultDes.bmp", bih.biWidth, bih.biHeight, bih.biBitCount, result)){
+				Console.WriteLine("画像復号化完了");
+			}
+		}
+	}
+}
